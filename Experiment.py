ca76367ee254fe3f9f2d98f0b8d4913ca9c37e9e
Sema Kuru <32938361+semakuru97@users.noreply.github.com>
2018-11-25 20:11:55 +0000

    Remove commented code, update to acceleration from voltage

--- Experiment.py
+++ Experiment.py
@@ -7,7 +7,7 @@ Created on Mon Nov 19 10:41:11 2018
 
 import numpy as np
 import pylab as pl
-#while True:
+
 with open('testonlyxdelay5.hex', 'r') as fp:
     hex_list = ["{:02x}".format(ord(c)) for c in fp.read()]
     print(hex_list)
@@ -15,45 +15,22 @@ with open('testonlyxdelay5.hex', 'r') as fp:
 x = []
 y = []
 z = []
-#t = []
 mark =[]
 #print(np.shape(hex_list)[0])
 for i in range(np.shape(hex_list)[0]):
     #print(int(hex_list[i], 16))
     if int(hex_list[i] , 16) == 255 and int(hex_list[i-3] , 16) != 255 and int(hex_list[i-2] , 16) != 255 and int(hex_list[i-1] , 16) != 255:
-        x_i = int(hex_list[i-3] , 16)
-        y_i = int(hex_list[i-2] , 16)
-        z_i = int(hex_list[i-1] , 16)#-28 
-        #t_i = int(hex_list[i-1] , 16)
-        keyi = int(hex_list[i] , 16)
+        x_i = int(hex_list[i-3] , 16) *3/255        
+        y_i = int(hex_list[i-2] , 16) *3/255
+        z_i = int(hex_list[i-1] , 16) *3/255
+        keyi = int(hex_list[i] , 16) *3/255
         x.append(x_i)
         y.append(y_i)
         z.append(z_i)
-       # t.append(t_i)
         mark.append(keyi)
-        
-# =============================================================================
-# for i in np.linspace(90 , 193 , 104):
-#     print(i)
-#     i = int(i)
-#     print(hex_list[i])
-#     x_i = int( hex_list[5*i+3] , 16)
-#     y_i = int( hex_list[5*i+4] , 16)
-#     z_i = int( hex_list[5*i+5] , 16)
-#     t_i = int( hex_list[5*i+6] , 16)-14
-#     mark_i = int( hex_list[5*i+7] , 16)
-#     x.append(x_i)
-#     y.append(y_i)
-#     z.append(z_i)
-#     t.append(t_i)
-#     mark.append(mark_i)
-# =============================================================================
-    
-#newx = np.linspace(0,3*9.8,5000)
-#for i in range(np.shape(x)[0]):
-    #newx[x[i]] = newx[i]
+  
 
-newt = np.arange(0, np.shape(x)[0]*1.05, 1.05)
+newt = np.arange(0, np.shape(x)[0]*0.13, 0.13)
 pl.plot(newt , x , '--')
 pl.plot(newt , y , '--')
 pl.plot(newt , z)
@@ -62,7 +39,7 @@ print(y)
 print(z)
 print(newt)
 print(mark)
-pl.ylim(0,50)
-pl.xlim(np.shape(newt)[0]-100, np.shape(newt)[0])
+#pl.ylim(0,50)
+#pl.xlim(np.shape(newt)[0]-100, np.shape(newt)[0])
 pl.xlabel('Time (s)')
-pl.ylabel('Voltage (mV)')
+pl.ylabel('acceleration (g)')
